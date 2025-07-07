c536afdeb915bd374b5548cca819bdf02ae9ed86
GreedyGaurav <gauravpinocchio786@gmail.com>
2025-07-07 20:40:18 +0530

    solve error

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "max-chat-two.vercel.app",
+      "https://max-chat-two.vercel.app",
     ],
     credentials: true,
   })
